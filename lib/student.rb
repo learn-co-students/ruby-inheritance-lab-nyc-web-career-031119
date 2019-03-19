46c925a8f4d9608a8513852378d958c3049ff982
Theo Weller <weller.theodor@gmail.com>
2019-03-19 23:10:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'pry'
+class Student < User
+  attr_accessor :knowledge
 
+def initialize
+  @knowledge = []
+end
+
+def learn(string)
+  self.knowledge << string
+end
+#binding.pry
+
+def knowledge
+  self.knowledge
+end
 end
